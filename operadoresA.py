3a0864a6c0780bbb8cd2a71bdeb98a12132acdc0
AngelGaarcia09 <a.22308051281193@cbtis128.edu.mx>
2024-09-20 09:31:34 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,57 @@
+print("Act 9 Clases v2 ")
+print("Angel Garcia Mat: 22308051281193")
+# zona de class
+class Operadores1193:
+    # zona de fuciones
+    def suma1193(self,A,G):
+        s1193=A+G
+        print(f"La suma de {A} + {G} = {s1193}")
+    # Resta
+    def resta1193(self,B,C):
+        r1193=B-C
+        print(f"La resta de {B} - {C} = {r1193}")
+    # Multiplicacion
+    def mult1193(self,D,E):
+        m1193=D*E
+        print(f"La multiplicacion de {D} * {E} = {m1193}")
+    # Divicion
+    def divi1193(self,F,H):
+        d1193=F/H
+        print(f"La divicion de {F} / {H} = {d1193}")
+    # Modulo
+    def Modu1193(self,I,J):
+        M1193=I%J
+        print(f" Modulo de {I} % {J} = {M1193}")
+    # Exponente
+    def Expo1193(self,K,L):
+        E1193=K**L
+        print(f" Exponente de {K} ** {L} = {E1193}")
+    # Cociente
+    def Co1193(self,N,O):
+        C1193=N//O
+        print(f" Cociente de {N} // {O} = {C1193}")
+
+# creacion de el objeto
+operagarcia=Operadores1193()
+
+# zona de uso de objetos
+print(" Funcion suma")
+operagarcia.suma1193(5,4)
+
+print(" Funcion resta")
+operagarcia.resta1193(10,4)
+
+print(" Funcion Multiplicacion")
+operagarcia.mult1193(3,3)
+
+print(" Funcion divicion")
+operagarcia.divi1193(20,5)
+
+print(" Funcion Modulo")
+operagarcia.Modu1193(8,7)
+
+print(" Funcion Exponente")
+operagarcia.Expo1193(9,2)
+
+print(" Funcion Cociente")
+operagarcia.Co1193(5,4)
